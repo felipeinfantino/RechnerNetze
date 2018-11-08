69eb8153d043f72e882f281d0beb51fdb84404f8
gnilebat <johannes.tabeling@gmx.de>
2018-11-08 13:57:00 +0100

    With time comparison

--- HA2/Johannes/Aufgabe6/clientudp.c
+++ HA2/Johannes/Aufgabe6/clientudp.c
@@ -8,7 +8,7 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <netdb.h>
-
+#include <time.h>
 
 int main(int argc, char *argv[]) {
 
@@ -18,10 +18,18 @@ int main(int argc, char *argv[]) {
     struct addrinfo hints, *servinfo, *p;
     struct sockaddr_storage their_addr;
     int rv;
-    int numbytes;
+    int numbytesS, numbytesR;
+
+    struct timespec start, stop;
+    double accum;
+
+    if (clock_gettime(CLOCK_REALTIME, &start) == -1) {
+        perror("clock gettime");
+        exit(EXIT_FAILURE);
+    }
 
     if (argc != 3) {
-        fprintf(stderr,"usage: talker hostname message\n");
+        fprintf(stderr, "usage: talker hostname message\n");
         exit(1);
     }
 
@@ -32,8 +40,9 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
         return 1;
     }
+
 // loop through all the results and make a socket
-    for(p = servinfo; p != NULL; p = p->ai_next) {
+    for (p = servinfo; p != NULL; p = p->ai_next) {
         if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
             perror("talker: socket");
             continue;
@@ -45,21 +54,34 @@ int main(int argc, char *argv[]) {
         return 2;
     }
     char *hello = "Hello Server";
-    if ((numbytes = sendto(sockfd, hello, strlen(hello), 0, p->ai_addr, p->ai_addrlen)) == -1) {
+    if ((numbytesS = sendto(sockfd, hello, strlen(hello), 0, p->ai_addr, p->ai_addrlen)) == -1) {
         perror("talker: sendto");
         exit(1);
     }
 
     char buf[512];
-    socklen_t addr_len = sizeof their_addr;
-    if ((numbytes = recvfrom(sockfd, buf, 100, 0, (struct sockaddr *)&their_addr, &addr_len)) == -1) {
-        perror("recvfrom");
+    socklen_t addr_len = sizeof(their_addr);
+    if ((numbytesS = recvfrom(sockfd, buf, strlen(buf), 0, (struct sockaddr *) &their_addr, &addr_len)) == -1) {
+        perror("talker: recvfrom");
         exit(1);
     }
-    printf("%s", buf);
 
     freeaddrinfo(servinfo);
-    printf("talker: sent %d bytes to %s\n", numbytes, argv[1]);
     close(sockfd);
+
+    if (clock_gettime(CLOCK_REALTIME, &stop) == -1) {
+        perror("clock gettime");
+        exit(EXIT_FAILURE);
+    }
+    //check if more than 1 passed and correct nanoseconds
+    struct timespec tmpTime;
+    if ((stop.tv_nsec - start.tv_nsec) < 0) {
+        tmpTime.tv_nsec = 1000000000 + stop.tv_nsec - start.tv_nsec; //add 1s in nanoseconds
+    } else {
+        tmpTime.tv_nsec = stop.tv_nsec - start.tv_nsec;
+    }
+    accum = tmpTime.tv_nsec / 1000; //convert to ms
+    printf("%lf ms\n", accum);
+
     return 0;
 }
