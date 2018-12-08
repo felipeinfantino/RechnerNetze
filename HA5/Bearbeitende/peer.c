54b3212aa49b7356ef522c5e4f6e4863d9582877
gnilebat <johannes.tabeling@gmx.de>
2018-12-08 14:48:46 +0100

    fixed segmentation fault

--- HA5/Bearbeitende/peer.c
+++ HA5/Bearbeitende/peer.c
@@ -10,14 +10,14 @@
 
 
 //------------------- Structs
-struct id_add_port{
+struct id_add_port {
     uint16_t id;
-    char* add;
+    char *add;
     uint16_t port;
 };
 
 
-struct peer{
+struct peer {
     struct id_add_port current;
     struct id_add_port vorganger;
     struct id_add_port nachfolger;
@@ -35,7 +35,7 @@ struct intern_hash_table_struct *hashtable = NULL;
 
 struct intern_hash_table_struct *find_value(char key[]) {
     struct intern_hash_table_struct *s;
-    HASH_FIND_STR(hashtable, key, s );
+    HASH_FIND_STR(hashtable, key, s);
     return s;
 }
 
@@ -45,24 +45,22 @@ struct intern_hash_table_struct *find_value(char key[]) {
 void read_header(char **pString, char *string, uint16_t *pInt, uint32_t *pInt1, uint16_t *pInt2, char **pString1,
                  unsigned char header[1000]);
 
-int isNthBitSet (unsigned char c, int n) {
+int isNthBitSet(unsigned char c, int n) {
     static unsigned char mask[] = {128, 64, 32, 16, 8, 4, 2, 1};
     return ((c & mask[n]) != 0);
 }
 
 
-unsigned int hash (const char *str, unsigned int length) {
+unsigned int hash(const char *str, unsigned int length) {
     unsigned int hash = 5381;
     unsigned int i = 0;
 
-    for (i = 0; i < length; ++str, ++i)
-    {
+    for (i = 0; i < length; ++str, ++i) {
         hash = ((hash << 5) + hash) + (*str);
     }
 
     return hash;
 }
-
 
 //------------------- Void Funktionen
 
@@ -85,15 +83,16 @@ unsigned int hash (const char *str, unsigned int length) {
 //   ID :
 //   IP :
 //   Port :
-void nachricht_ausgeben(struct peer* current_peer, int internal, char* art, uint16_t id_absender, uint32_t ip_absender, uint16_t port_absender){
+void nachricht_ausgeben(struct peer *current_peer, int internal, char *art, uint16_t id_absender, uint32_t ip_absender,
+                        uint16_t port_absender) {
     printf("Internal : %i\n", internal);
     printf("Art der Nachricht : %s\n", art);
     printf("Absender : \n\t ID : %u\n\t IP : %u\n\t PORT : %u\n\n", id_absender, ip_absender, port_absender);
-    printf("Vorgänger : \n\t ID : %s\n\t IP : %s\n\t PORT : %s\n\n", current_peer->vorganger.id, current_peer->vorganger.add, current_peer->vorganger.port);
-    printf("Nachfolger : \n\t ID : %s\n\t IP : %s\n\t PORT : %s\n\n", current_peer->nachfolger.id, current_peer->nachfolger.add, current_peer->nachfolger.port);
+    printf("Vorgänger : \n\t ID : %u\n\t IP : %s\n\t PORT : %u\n\n", current_peer->vorganger.id,
+           current_peer->vorganger.add, current_peer->vorganger.port);
+    printf("Nachfolger : \n\t ID : %u\n\t IP : %s\n\t PORT : %u\n\n", current_peer->nachfolger.id,
+           current_peer->nachfolger.add, current_peer->nachfolger.port);
 }
-
-
 
 
 void nachricht_bearbeiten(int client_socket, unsigned char receive_header[], unsigned char answer_header[],
@@ -157,8 +156,9 @@ void nachricht_bearbeiten(int client_socket, unsigned char receive_header[], uns
     // }
 }
 
-void nachricht_weiterleiten(int client_socket, int internal ,unsigned char receive_header[], unsigned char answer_header[],
-                          unsigned int key_length, const char *key, const char *value) {
+void
+nachricht_weiterleiten(int client_socket, int internal, unsigned char receive_header[], unsigned char answer_header[],
+                       unsigned int key_length, const char *key, const char *value) {
     //TODO Wenn internal bit 0 ist, dann muss auf 1 gesetzt werden, nachricht ggf anpassen und weiterleiten an dem nachfolger
 }
 
@@ -166,8 +166,8 @@ void nachricht_weiterleiten(int client_socket, int internal ,unsigned char recei
 void add_value(char key[], char value[]) {
     struct intern_hash_table_struct *s = NULL;
     HASH_FIND_STR(hashtable, key, s);  /* id already in the hash? */
-    if (s==NULL) {
-        s = (struct intern_hash_table_struct *)malloc(sizeof *s);
+    if (s == NULL) {
+        s = (struct intern_hash_table_struct *) malloc(sizeof *s);
         s->key = (char *) malloc(strlen(key) + 1);
         s->value = (char *) malloc(strlen(value) + 1);
         strncpy(s->key, key, strlen(key));
@@ -175,8 +175,6 @@ void add_value(char key[], char value[]) {
         HASH_ADD_STR(hashtable, key, s);  /* id: name of key field */
     }
 }
-
-
 
 
 void delete_value(struct intern_hash_table_struct *s) {
@@ -187,7 +185,7 @@ void delete_value(struct intern_hash_table_struct *s) {
 void str_to_uint16(const char *str, uint16_t *res) {
     char *end;
     long val = strtol(str, &end, 10);
-    *res = (uint16_t)val;
+    *res = (uint16_t) val;
 }
 
 
@@ -196,55 +194,49 @@ void str_to_uint16(const char *str, uint16_t *res) {
 
 //------------------- Main
 
-int main(int argc, char *argv[])
-{
-    //check for correct number of arguments
-    if (argc != 10)
-    {
+int main(int argc, char *argv[]) {
+    if (argc != 10) {
         perror("Wrong input format");
         exit(1);
     }
-
-    //Bsp aufruf ./peer 15 127.0.0.1 4711 245 127.0.0.1 4710 112 127.0.0.1 4712
-    //Initalize peer
-    struct peer* current_peer = malloc(sizeof(current_peer));
+    struct peer *current_peer = malloc(sizeof(struct peer));
 
     //initialisiere structs
-    current_peer->current = (struct id_add_port){.id= 0, .add = malloc(strlen(argv[2])) , .port=0};
-    current_peer->vorganger = (struct id_add_port){.id= 0, .add = malloc(strlen(argv[5])) , .port=0};
-    current_peer->nachfolger = (struct id_add_port){.id= 0, .add = malloc(strlen(argv[8])) , .port= 0};
+    current_peer->current = (struct id_add_port) {.id= 0, .add = malloc(strlen(argv[2])), .port=0};
+    current_peer->vorganger = (struct id_add_port) {.id= 0, .add = malloc(strlen(argv[5])), .port=0};
+    current_peer->nachfolger = (struct id_add_port) {.id= 0, .add = malloc(strlen(argv[8])), .port= 0};
 
-    //Kopiere eingaben in den structs, bei id und port sind ja uint16 und add is string
     //current
     str_to_uint16(argv[1], &current_peer->current.id);
     strncpy(current_peer->current.add, argv[2], strlen(argv[2]));
     str_to_uint16(argv[3], &current_peer->current.port);
-    //vorganger
+
+    //Vorganger
     str_to_uint16(argv[4], &current_peer->vorganger.id);
     strncpy(current_peer->vorganger.add, argv[5], strlen(argv[5]));
     str_to_uint16(argv[6], &current_peer->vorganger.port);
 
+    //Nachfolger
     str_to_uint16(argv[7], &current_peer->nachfolger.id);
     strncpy(current_peer->nachfolger.add, argv[8], strlen(argv[8]));
     str_to_uint16(argv[9], &current_peer->nachfolger.port);
 
-    printf("Id %u\n",current_peer->current.id);
-    printf("Add %s\n",current_peer->current.add);
-    printf("Port %u\n",current_peer->current.port);
+    printf("Id %u\n", current_peer->current.id);
+    printf("Add %s\n", current_peer->current.add);
+    printf("Port %u\n", current_peer->current.port);
 
-    printf("Id %u\n",current_peer->vorganger.id);
-    printf("Add %s\n",current_peer->vorganger.add);
-    printf("Port %u\n",current_peer->vorganger.port);
+    printf("Id %u\n", current_peer->vorganger.id);
+    printf("Add %s\n", current_peer->vorganger.add);
+    printf("Port %u\n", current_peer->vorganger.port);
 
-    printf("Id %u\n",current_peer->nachfolger.id);
-    printf("Add %s\n",current_peer->nachfolger.add);
-    printf("Port %u\n",current_peer->nachfolger.port);
+    printf("Id %u\n", current_peer->nachfolger.id);
+    printf("Add %s\n", current_peer->nachfolger.add);
+    printf("Port %u\n", current_peer->nachfolger.port);
 
-    if(current_peer->current.id == 0 || current_peer->vorganger.id == 0 || current_peer->nachfolger.id == 0){
+    if (current_peer->current.id == 0 || current_peer->vorganger.id == 0 || current_peer->nachfolger.id == 0) {
         perror("Id from one of the peers is 0, that is wrong");
         exit(1);
     }
-
 
     //declare needed structures
     struct addrinfo server_info_config;
@@ -256,12 +248,11 @@ int main(int argc, char *argv[])
     server_info_config.ai_family = AF_INET;
     server_info_config.ai_socktype = SOCK_STREAM;
 
-    char* portInChar = malloc(4);
-    sprintf (portInChar, "%u", current_peer->current.port);
-    
+    char *portInChar = malloc(4);
+    sprintf(portInChar, "%u", current_peer->current.port);
+
     //get host information and load it into *results
-    if (getaddrinfo(current_peer->current.add, portInChar, &server_info_config, &results) != 0)
-    {
+    if (getaddrinfo(current_peer->current.add, portInChar, &server_info_config, &results) != 0) {
         perror("Error in getAddinfo");
         exit(1);
     }
@@ -270,54 +261,239 @@ int main(int argc, char *argv[])
     int server_socket = socket(results->ai_family, results->ai_socktype, results->ai_protocol);
 
     //bind the new socket to a port
-    if (bind(server_socket, results->ai_addr, results->ai_addrlen) == -1)
-    {
+    if (bind(server_socket, results->ai_addr, results->ai_addrlen) == -1) {
         perror("Couldn't bind the socket to this port");
         exit(1);
-    }else{
+    } else {
+        printf("bind succ\n");
+    }
+
+    //prepare the socket for incoming connections
+    if (listen(server_socket, 10) == -1) {
+        perror("Error while listening");
+        exit(1);
+    } else {
+        printf("listen succ\n");
+    }
+    printf("start while loop\n");
+
+    while (1) {
+        //construct sockaddr_storage for storing client information
+        struct sockaddr_storage client_info;
+        socklen_t client_info_length = sizeof(client_info);
+        //accept the newest connection and load it's data to the client_info struct
+        int client_socket = accept(server_socket, (struct sockaddr *) &client_info, &client_info_length);
+        if (client_socket == -1) {
+            perror("Error while accepting the connection");
+            exit(1);
+        } else {
+            printf("client accepted\n");
+        }
+
+        unsigned char receive_header[1000];
+        unsigned char answer_header[1000];
+
+        if (recv(client_socket, &receive_header, 1000, 0) == -1) //receive header
+        {
+            perror("Error receiving");
+            exit(1);
+        } else {
+            printf("recv success\n");
+        }
+
+
+        //Nach dem wir die Nachricht empfangen haben, hashen wir den Key
+        //TODO Header lesen. Wir müssen anders als vorherige aufgabe machen
+        char* key;
+        char transaktions_id;
+        uint16_t id_absender;
+        uint16_t key_length;
+        uint16_t port_absender;
+        uint32_t ip_absender;
+        char* value;
+
+        printf("Header %s", receive_header);
+        //read_header(&key, &transaktions_id, &id_absender, &ip_absender, &port_absender, &value,receive_header);
+
+        //Bereite die ausgabe vor
+        int internal = isNthBitSet(receive_header[0], 1);
+
+        char* art;
+        int isGet = isNthBitSet(receive_header[0], 5);
+        int isSet = isNthBitSet(receive_header[0], 6);
+        int isDelete = isNthBitSet(receive_header[0], 7);
+
+        if(isGet) strcpy(art, "GET");
+        if(isSet) strcpy(art, "SET");
+        if(isDelete) strcpy(art, "DELETE");
+
+        if(art == NULL){
+            perror("Wrong usage, only set or get or delete.");
+            exit(1);
+        }
+
+        nachricht_ausgeben(current_peer, internal, art, id_absender, ip_absender, port_absender);
+        /*
+        //Hash der Key und gucke ob das zu dem aktuellen peer gehört
+        unsigned int hash_value = hash(key, (key_length+1));
+
+        //Prüfen ob den aktuellen peer zuständig für die anfrage ist
+        //Wenn ja, bearbeite die anfrage (also führe set, get, delete in interne hastable aus)
+        //Wenn nicht setzte das intern bit zu 1 und leite dass zu nachfolger weiter
+
+        // 2 Fälle
+        // Fall 1 : hash liegt zwischen den letzen und dem ersten Knoten
+        // Bsp letzte knoten 220 und ersten 10
+        // Fall 2 : hash liegt zwischen zwei normalen knoten.
+
+
+        //Fall 1 (ersten Knoten): wir gucken ob der current peer ein kleineres ID hat als vorgänger, wenn ja es ist Fall 1 ansonsten Fall 2
+
+        if(atoi(current_peer->current.id) < atoi(current_peer->vorganger.id)){
+            if(hash_value < atoi(current_peer->current.id) || hash_value > atoi(current_peer->vorganger.id)){
+                //Dann current ist dafür zuständig
+
+                //TODO Nachricht bearbeiten und an absender zurückschicken
+                nachricht_bearbeiten(client_socket, receive_header, answer_header, key_length, key, value);
+
+            }else{
+                //Nicht zuständig, dann weiter leiten
+
+                //TODO Nachricht weiterleiten
+                nachricht_weiterleiten(client_socket, internal ,receive_header, answer_header, key_length, key, value);
+            }
+
+        }else{
+            //Fall 2: also normalen Fall
+            if(hash_value > atoi(current_peer->current.id) || hash_value < atoi(current_peer->vorganger.id)){
+                // Nachricht weiterleiten
+            }else{
+                // Nachricht bearbeiten
+            }
+        }
+
+        //close(client_socket);
+         */
+    }
+    close(server_socket);
+    freeaddrinfo(results);
+}
+
+
+
+/*    //check for correct number of arguments
+    printf("0");
+    if (argc != 10) {
+        perror("Wrong input format");
+        exit(1);
+    }
+    //Bsp aufruf ./peer 15 127.0.0.1 4711 245 127.0.0.1 4710 112 127.0.0.1 4712
+    //Initalize peer
+
+    struct peer *current_peer = malloc(sizeof(current_peer));
+
+    //initialisiere structs
+    current_peer->current = (struct id_add_port) {.id= 0, .add = malloc(strlen(argv[2])), .port=0};
+    current_peer->vorganger = (struct id_add_port) {.id= 0, .add = malloc(strlen(argv[5])), .port=0};
+    current_peer->nachfolger = (struct id_add_port) {.id= 0, .add = malloc(strlen(argv[8])), .port= 0};
+
+    //Kopiere eingaben in den structs, bei id und port sind ja uint16 und add is string
+    //current
+    str_to_uint16(argv[1], &current_peer->current.id);
+    strncpy(current_peer->current.add, argv[2], strlen(argv[2]));
+    str_to_uint16(argv[3], &current_peer->current.port);
+    //Vorganger
+    str_to_uint16(argv[4], &current_peer->vorganger.id);
+    strncpy(current_peer->vorganger.add, argv[5], strlen(argv[5]));
+    str_to_uint16(argv[6], &current_peer->vorganger.port);
+    //Nachfolger
+    str_to_uint16(argv[7], &current_peer->nachfolger.id);
+    strncpy(current_peer->nachfolger.add, argv[8], strlen(argv[8]));
+    str_to_uint16(argv[9], &current_peer->nachfolger.port);
+
+    printf("Id %u\n", current_peer->current.id);
+    printf("Add %s\n", current_peer->current.add);
+    printf("Port %u\n", current_peer->current.port);
+
+    printf("Id %u\n", current_peer->vorganger.id);
+    printf("Add %s\n", current_peer->vorganger.add);
+    printf("Port %u\n", current_peer->vorganger.port);
+
+    printf("Id %u\n", current_peer->nachfolger.id);
+    printf("Add %s\n", current_peer->nachfolger.add);
+    printf("Port %u\n", current_peer->nachfolger.port);
+
+    if (current_peer->current.id == 0 || current_peer->vorganger.id == 0 || current_peer->nachfolger.id == 0) {
+        perror("Id from one of the peers is 0, that is wrong");
+        exit(1);
+    }
+
+    //declare needed structures
+    struct addrinfo server_info_config;
+    struct addrinfo *results;
+
+    //initialize server_info_config
+    memset(&server_info_config, 0, sizeof(server_info_config));
+    server_info_config.ai_protocol = IPPROTO_TCP;
+    server_info_config.ai_family = AF_INET;
+    server_info_config.ai_socktype = SOCK_STREAM;
+
+    char *portInChar = malloc(4);
+    sprintf(portInChar, "%u", current_peer->current.port);
+
+    //get host information and load it into *results
+    if (getaddrinfo(current_peer->current.add, portInChar, &server_info_config, &results) != 0) {
+        perror("Error in getAddinfo");
+        exit(1);
+    }
+
+    //create new socket using data obtained with getaddrinfo()
+    int server_socket = socket(results->ai_family, results->ai_socktype, results->ai_protocol);
+
+    //bind the new socket to a port
+    if (bind(server_socket, results->ai_addr, results->ai_addrlen) == -1) {
+        perror("Couldn't bind the socket to this port");
+        exit(1);
+    } else {
         printf("bind suc");
     }
 
     //prepare the socket for incoming connections
-    if (listen(server_socket, 10) == -1)
-    {
+    if (listen(server_socket, 10) == -1) {
         perror("Error while listening");
         exit(1);
-    }else{
+    } else {
         printf("listen succ");
     }
     printf("check");
 
     //checkpoint
     //start the infinite loop
-    while (1)
-    {
+     while (1) {
         printf("holll");
         printf("entering while");
         //construct sockaddr_storage for storing client information
         struct sockaddr_storage client_info;
         socklen_t client_info_length = sizeof(client_info);
         //aceept the newest connection and load it's data to the client_info struct
-        int client_socket = accept(server_socket, (struct sockaddr *)&client_info, &client_info_length);
-        if (client_socket == -1)
-        {
+        int client_socket = accept(server_socket, (struct sockaddr *) &client_info, &client_info_length);
+        if (client_socket == -1) {
             perror("Error while accepting the connection");
             exit(1);
-        }else{
+        } else {
             printf("succ");
         }
 
         unsigned char receive_header[1000];
         unsigned char answer_header[1000];
-        
+
         if (recv(client_socket, &receive_header, 1000, 0) == -1) //receive header
         {
             perror("Error receiving ");
             exit(1);
-        }else{
+        } else {
             printf("succccs");
         }
-
         /*
 
 
@@ -394,45 +570,45 @@ int main(int argc, char *argv[])
             }
         }
 
-        */
-
         //close(client_socket);
     }
+
     //close(server_socket);
     //freeaddrinfo(results);
-}
+}*/
 
-void read_header(char **key, char *transaktions_id, uint16_t *id_absender, uint32_t *ip_absender, uint16_t *port_absender, char **value,
-                 unsigned char* header) {
+void read_header(char **key, char *transaktions_id, uint16_t *id_absender, uint32_t *ip_absender, uint16_t *port_absender,
+            char **value,
+            unsigned char *header) {
 
     //Länge von key und value holen
     uint32_t key_length;
     uint32_t value_length;
 
     //Lesen länge von value und key und tauchen wir den byte order
-    memcpy(&key_length, header+2, 2);
-    memcpy(&value_length, header+4, 2);
+    memcpy(&key_length, header + 2, 2);
+    memcpy(&value_length, header + 4, 2);
 
     key_length = ntohl(key_length);
     value_length = ntohl(key_length);
 
     //Header lesen und speichern in den deklarierten Variablen, da wir schon ein pointer als parameter haben
     // übergeben das einfach in memcpy
-    memcpy(transaktions_id, header+1, 1);
-    memcpy(id_absender, header+6, 2);
-    memcpy(ip_absender, header+8, 4);
-    memcpy(port_absender, header+10, 2);
+    memcpy(transaktions_id, header + 1, 1);
+    memcpy(id_absender, header + 6, 2);
+    memcpy(ip_absender, header + 8, 4);
+    memcpy(port_absender, header + 10, 2);
 
-    if(key_length > 0){
-        *key = (char*)malloc(key_length +1);
-        memcpy(*key, header+14, key_length);
-    }else{
+    if (key_length > 0) {
+        *key = (char *) malloc(key_length + 1);
+        memcpy(*key, header + 14, key_length);
+    } else {
         *key = NULL;
     }
-    if(value_length > 0){
-        *value = (char*)malloc(value_length +1);
-        memcpy(*key, header+14+key_length, value_length);
-    } else{
+    if (value_length > 0) {
+        *value = (char *) malloc(value_length + 1);
+        memcpy(*key, header + 14 + key_length, value_length);
+    } else {
         *value = NULL;
     }
     //TODO vielleicht die nullterminierende chars nach memcpy
