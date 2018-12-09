cbd459552a02a43f4e5d560c8511a082521d215c
felipeinfantino <felipe.infantino@hotmail.com>
2018-12-09 16:35:22 +0100

    read header implementiert

--- HA5/Bearbeitende/peer.c
+++ HA5/Bearbeitende/peer.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <memory.h>
 #include <unistd.h>
+#include <arpa/inet.h>
 #include "uthash.h"
 
 #define HEAD 6
@@ -41,9 +42,6 @@ struct intern_hash_table_struct *find_value(char key[]) {
 
 //------------------- Funktionen die int zurückgeben
 
-
-void read_header(char **pString, char *string, uint16_t *pInt, uint32_t *pInt1, uint16_t *pInt2, char **pString1,
-                 unsigned char header[1000]);
 
 int isNthBitSet(unsigned char c, int n) {
     static unsigned char mask[] = {128, 64, 32, 16, 8, 4, 2, 1};
@@ -83,11 +81,11 @@ unsigned int hash(const char *str, unsigned int length) {
 //   ID :
 //   IP :
 //   Port :
-void nachricht_ausgeben(struct peer *current_peer, int internal, char *art, uint16_t id_absender, uint32_t ip_absender,
+void nachricht_ausgeben(struct peer *current_peer, int internal, char *art, uint16_t id_absender, char* ip_absender,
                         uint16_t port_absender) {
     printf("Internal : %i\n", internal);
     printf("Art der Nachricht : %s\n", art);
-    printf("Absender : \n\t ID : %u\n\t IP : %u\n\t PORT : %u\n\n", id_absender, ip_absender, port_absender);
+    printf("Absender : \n\t ID : %u\n\t IP : %s\n\t PORT : %u\n\n", id_absender, ip_absender, port_absender);
     printf("Vorgänger : \n\t ID : %u\n\t IP : %s\n\t PORT : %u\n\n", current_peer->vorganger.id,
            current_peer->vorganger.add, current_peer->vorganger.port);
     printf("Nachfolger : \n\t ID : %u\n\t IP : %s\n\t PORT : %u\n\n", current_peer->nachfolger.id,
@@ -188,8 +186,88 @@ void str_to_uint16(const char *str, uint16_t *res) {
     *res = (uint16_t) val;
 }
 
+void read_header_peer(char **key, char *transaktions_id, uint16_t *id_absender, char **ip_absender, uint16_t *port_absender,
+            char **value,
+            unsigned char *header) {
 
 
+    //Länge von key und value holen
+    uint16_t key_length;
+    uint16_t value_length;
+
+    //Lesen länge von value und key und tauchen wir den byte order
+    memcpy(&key_length, header + 2, 2);
+    memcpy(&value_length, header + 4, 2);
+
+    // key_length = ntohs(key_length);
+    // value_length = ntohs(value_length);
+
+    printf("Key length %u ", key_length);
+    printf("Value length %u ", value_length);
+
+
+    //Header lesen und speichern in den deklarierten Variablen, da wir schon ein pointer als parameter haben
+    // übergeben das einfach in memcpy
+    memcpy(transaktions_id, header + 1, 1);
+    memcpy(id_absender, header + 6, 2);
+    memcpy(port_absender, header + 12, 2);
+    *ip_absender = malloc(4);
+    memcpy(ip_absender, header + 8, 4);
+    uint16_t test = ntohs(*id_absender);
+    printf("T id : %s\n",  transaktions_id);
+     printf("Id : %u\n", *id_absender);
+      printf("Ip : %s\n",*ip_absender);
+       printf("Port : %u\n", *port_absender);
+
+    //Hier Valgrind meckert wegen conditional jump or move depends on uninitialised value
+    if ( key_length > 0) {
+        *key = (char *) malloc(key_length +1);
+        memcpy(*key, header + 14, key_length);
+        key[key_length] = '\0';
+        if(*key != NULL){
+        printf("Key :  %s\n", *key);
+        }
+    } else {
+        *key = NULL;
+    }
+    if (value_length > 0) {
+        *value = (char *) malloc(value_length +1 );
+        memcpy(*value, header + 14 + key_length, value_length);
+        value[value_length] = '\0';
+        printf("Value : %s\n", *value);
+    } else {
+        *value = NULL;
+    }
+}
+
+void read_header_client(struct peer* current_peer,char **key, char *transaktions_id, uint16_t *id_absender, char** ip_absender, uint16_t *port_absender,
+            char **value,
+            unsigned char *header) {
+
+        int key_length = (header[2] << 8) + header[3];   //process the key length
+        int value_length = (header[4] << 8) + header[5]; //process the value length
+    
+        *key = malloc(key_length + 1);
+        *value = malloc(value_length + 1);
+        *ip_absender = malloc(strlen(current_peer->current.add) +1);
+
+        //Copy value and key
+        memcpy(*key, &header[6], key_length);
+        memcpy(*value, &header[6 + key_length], value_length);
+       
+        key[key_length] = '\0';
+        value[value_length] = '\0';
+
+        //Copy values in poiners
+        memcpy(id_absender, &current_peer->current.id, 2);
+        memcpy(port_absender, &current_peer->current.port, 2);
+        strcpy(*ip_absender, current_peer->current.add);
+
+        printf("Id absender : %u\n", *id_absender);
+        printf("Port absender %u\n", *port_absender);
+        printf("Ip absender : %s\n", *ip_absender);
+       
+  }
 
 
 
@@ -203,25 +281,28 @@ int main(int argc, char *argv[]) {
     struct peer *current_peer = malloc(sizeof(struct peer));
 
     //initialisiere structs
-    current_peer->current = (struct id_add_port) {.id= 0, .add = malloc(strlen(argv[2])), .port=0};
-    current_peer->vorganger = (struct id_add_port) {.id= 0, .add = malloc(strlen(argv[5])), .port=0};
-    current_peer->nachfolger = (struct id_add_port) {.id= 0, .add = malloc(strlen(argv[8])), .port= 0};
+    current_peer->current = (struct id_add_port) {.id= 0, .add = malloc(strlen(argv[2]) +1), .port=0};
+    current_peer->vorganger = (struct id_add_port) {.id= 0, .add = malloc(strlen(argv[5])+1), .port=0};
+    current_peer->nachfolger = (struct id_add_port) {.id= 0, .add = malloc(strlen(argv[8])+1), .port= 0};
 
     //TODO sprintf to copy full adress
     // //current
     str_to_uint16(argv[1], &current_peer->current.id);
-    sprintf(current_peer->current.add, "%.4s", argv[2]);
+    sprintf(current_peer->current.add, "%.9s", argv[2]);
     str_to_uint16(argv[3], &current_peer->current.port);
 
     // //Vorganger
     str_to_uint16(argv[4], &current_peer->vorganger.id);
-    sprintf(current_peer->vorganger.add, "%.4s", argv[5]);
+    sprintf(current_peer->vorganger.add, "%.9s", argv[5]);
     str_to_uint16(argv[6], &current_peer->vorganger.port);
 
     // //Nachfolger
     str_to_uint16(argv[7], &current_peer->nachfolger.id);
-    sprintf(current_peer->nachfolger.add, "%.4s", argv[8]);
+    sprintf(current_peer->nachfolger.add, "%.9s", argv[8]);
     str_to_uint16(argv[9], &current_peer->nachfolger.port);
+
+    printf("Lengths %zu , %zu , %zu \n", strlen(argv[2]), strlen(argv[5]), strlen(argv[8]));
+
 
     printf("Id %u\n", current_peer->current.id);
     printf("Add %s\n", current_peer->current.add);
@@ -302,57 +383,76 @@ int main(int argc, char *argv[]) {
             printf("recv success\n");
         }
 
+        int internal = isNthBitSet(receive_header[0], 1);
 
-     //Nach dem wir die Nachricht empfangen haben, hashen wir den Key
+        char* art;
+        int isGet = isNthBitSet(receive_header[0], 5);
+        int isSet = isNthBitSet(receive_header[0], 6);
+        int isDelete = isNthBitSet(receive_header[0], 7);
+
+        if(isGet) {
+            art = malloc(3);
+            strcpy(art, "GET");
+        }
+        if(isSet) {
+            art = calloc(3,3);
+            strcpy(art, "SET");
+        }
+        if(isDelete) {
+            art = calloc(6,6);
+            strcpy(art, "DELETE");
+        }
+
+        if(art == NULL){
+            perror("Wrong usage, only set or get or delete.");
+            exit(1);
+        }
+
+        //Wenn es intern ist, dann lesen wir read_header_peer, andersfalls lesen wir read_header_client
 
         char* key;
         char transaktions_id;
-        uint16_t id_absender;
-        uint16_t key_length;
+        uint16_t id_absender;    
         uint16_t port_absender;
-        uint32_t ip_absender;
+        char* ip_absender;
         char* value;
 
+        if(!internal){
+    
+        uint16_t key_length = 6;
+        uint16_t value_length = 9;
+        uint16_t id = 34;
+        uint16_t port = 4111;
+        strcpy(ip_absender, "127.0.0.1");
+        strcpy(key, "Felipe");
+        strcpy(value, "Infantino");
+
+        //Mock header
+        unsigned char mock_header[key_length + value_length + 14]; // 14 denn siehe protokoll
+        memset(mock_header, 0, key_length + value_length + 14);
+        mock_header[0] = 0b10000010; // erste bit ist intern und letzten 3 set,get , delete
+        mock_header[1] = 0b00000011; // transaktion ID
+        // add key_length to answer
+        memcpy(&mock_header[2], &key_length, 2); //Keylengt
+        memcpy(&mock_header[4], &value_length, 2); // value length
+        memcpy(&mock_header[6], &id, 2); //id 
+        memcpy(&mock_header[8], &ip_absender, 4); //ip 
+        memcpy(&mock_header[12], &port, 2); //port
+        memcpy(&mock_header[14], &key, key_length);
+        memcpy(&mock_header[key_length+14], &value, value_length);
+
+        
         // //TODO Header lesen. Wir müssen anders als vorherige aufgabe machen
-        read_header(&key, &transaktions_id, &id_absender, &ip_absender, &port_absender, &value,receive_header);
+        //read_header_peer(&key, &transaktions_id, &id_absender, &ip_absender, &port_absender, &value,mock_header);
+        }else{
+           read_header_client(current_peer,&key, &transaktions_id, &id_absender, &ip_absender, &port_absender, &value,receive_header);
+        }
 
-        // //Bereite die ausgabe vor
-        // int internal = isNthBitSet(receive_header[0], 1);
+        //nachricht_ausgeben(current_peer, internal, art, id_absender, ip_absender, port_absender);
 
-        // char* art;
-        // int isGet = isNthBitSet(receive_header[0], 5);
-        // int isSet = isNthBitSet(receive_header[0], 6);
-        // int isDelete = isNthBitSet(receive_header[0], 7);
-
-        // if(isGet) {
-        //     art = calloc(strlen("GET"), sizeof(char));
-        //     strcpy(art, "GET");
-        // }
-        // if(isSet) {
-        //     art = calloc(strlen("SET"), sizeof(char));
-        //     strcpy(art, "SET");
-        // }
-        // if(isDelete) {
-        //     art = calloc(strlen("DELETE"), sizeof(char));
-        //     strcpy(art, "DELETE");
-        // }
-
-        // if(art == NULL){
-        //     perror("Wrong usage, only set or get or delete.");
-        //     exit(1);
-        // }
-
-
-
-
-
-
-
-
-    //     nachricht_ausgeben(current_peer, internal, art, id_absender, ip_absender, port_absender);
-    //     //Hash der Key und gucke ob das zu dem aktuellen peer gehört
-    //     printf("hashs key: %s with length+1: %u\n", key, key_length+1);
-        //unsigned int hash_value = hash(key, (key_length+1)); //TODO read header before that
+        // //Hash der Key und gucke ob das zu dem aktuellen peer gehört
+        // printf("hashs key: %s with length+1: %u\n", key, key_length+1);
+        // unsigned int hash_value = hash(key, (key_length+1)); //TODO read header before that
 
 /*
         //Prüfen ob den aktuellen peer zuständig für die anfrage ist
@@ -594,50 +694,7 @@ int main(int argc, char *argv[]) {
     //freeaddrinfo(results);
 }*/
 
-void read_header(char **key, char *transaktions_id, uint16_t *id_absender, uint32_t *ip_absender, uint16_t *port_absender,
-            char **value,
-            unsigned char *header) {
 
-    printf("Hello read");
-
-    //Länge von key und value holen
-    uint32_t key_length;
-    uint32_t value_length;
-
-    //Lesen länge von value und key und tauchen wir den byte order
-    memcpy(&key_length, header + 2, 2);
-    memcpy(&value_length, header + 4, 2);
-
-    // key_length = ntohl(key_length);
-    // value_length = ntohl(key_length);
-
-    printf("Key length %u ", key_length);
-    printf("Value length %u ", value_length);
-
-
-    // //Header lesen und speichern in den deklarierten Variablen, da wir schon ein pointer als parameter haben
-    // // übergeben das einfach in memcpy
-    // memcpy(transaktions_id, header + 1, 1);
-    // memcpy(id_absender, header + 6, 2);
-    // memcpy(ip_absender, header + 8, 4);
-    // memcpy(port_absender, header + 10, 2);
-
-    // if (key_length > 0) {
-    //     *key = (char *) malloc(key_length + 1);
-    //     memcpy(*key, header + 14, key_length);
-    // } else {
-    //     *key = NULL;
-    // }
-    // if (value_length > 0) {
-    //     *value = (char *) malloc(value_length + 1);
-    //     memcpy(*key, header + 14 + key_length, value_length);
-    // } else {
-    //     *value = NULL;
-    // }
- printf("Hello read2");
-
-    //TODO vielleicht die nullterminierende chars nach memcpy
-}
 
 
 
